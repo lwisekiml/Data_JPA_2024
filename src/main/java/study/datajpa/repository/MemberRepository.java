e9969112be4f9a4260edb12bd883512aeddfa8c9
wisekim <lwisekiml@naver.com>
2024-04-16 20:45:02 +0900

    스프링 데이터 JPA 페이징과 정렬 : Page로 원복

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,7 +2,6 @@ package study.datajpa.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -36,5 +35,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findMemberByUsername(String username); // 단건
     Optional<Member> findOptionalByUsername(String username); // 단건
 
-    List<Member> findByAge(int age, Pageable pageable);
+    Page<Member> findByAge(int age, Pageable pageable);
 }
