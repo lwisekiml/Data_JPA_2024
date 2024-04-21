8ce91459441b42705af7a7839b2aab3f9d94e4fd
wisekim <lwisekiml@naver.com>
2024-04-21 19:48:34 +0900

    Auditing : 등록자, 수정자 추가
    - 보통 시간을 많이 사용하지만 등록자, 수정자는 안쓰는 테이블을 있을 수 있어서 이와같이 상속으로 해결할 수 있다.

--- src/main/java/study/datajpa/entity/BaseTImeEntity.java
+++ src/main/java/study/datajpa/entity/BaseTImeEntity.java
@@ -4,20 +4,22 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseEntity extends BaseTImeEntity {
+public class BaseTImeEntity {
 
-    @CreatedBy
+    @CreatedDate
     @Column(updatable = false)
-    private String createdBy;
+    private LocalDateTime createdDate;
 
-    @LastModifiedBy
-    private String lastModifiedBy;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
 }
