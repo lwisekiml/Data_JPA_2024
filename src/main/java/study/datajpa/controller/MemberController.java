918a2a8d76179912d1e59260eb96e1b6a857eea7
wisekim <lwisekiml@naver.com>
2024-04-23 14:06:19 +0900

    Web 확장 - 페이징과 정렬 : 기본 설정 (20개)

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,8 +1,9 @@
 package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +27,18 @@ public class MemberController {
         return member.getUsername();
     }
 
+    // http://localhost:8080/members?page=0&size=3&sort=id,desc
+    // 기본 설정은 20개씩 나오도록 되어 있다.
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
