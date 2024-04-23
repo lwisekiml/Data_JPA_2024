ecea8f18802c84ab90da8fc3f6fcf140cafb5230
wisekim <lwisekiml@naver.com>
2024-04-23 14:09:44 +0900

    Web 확장 - 페이징과 정렬 : 특정한 곳 페이지 설정 방법

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +31,7 @@ public class MemberController {
     // http://localhost:8080/members?page=0&size=3&sort=id,desc
     // 기본 설정은 20개씩 나오도록 되어 있다.
     @GetMapping("/members")
-    public Page<Member> list(Pageable pageable) {
+    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable) { // 특별한 곳에 설정 변경
         Page<Member> page = memberRepository.findAll(pageable);
         return page;
     }
