3d12ba045d84c15cf2598b7099c2a26e5b65c968
wisekim <lwisekiml@naver.com>
2024-05-02 19:21:21 +0900

    네이티브 쿼리 1

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -393,4 +393,23 @@ public class MemberRepositoryTest {
             System.out.println("teamName = " + teamName);
         }
     }
+
+    @Test
+    public void nativeQuery() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        Member result = memberRepository.findByNativeQuery("m1");
+        System.out.println("result = " + result);
+    }
 }
